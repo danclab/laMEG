5ad61658a28492ef4ebc55d88be812506694f164
jbonaiuto <jbonaiuto@gmail.com>
2024-01-08 08:10:20 +0100

    variable renaming, use non-mutable default parameters, split inversion and coregistration, allow gain matrices to be precomputed, all Hann window to be used or not for sliding window reconstruction, support for simulating multiple trials (allow simulated data to be averaged over trials)

--- laminar.py
+++ laminar.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-from invert import invert_ebb, invert_msp, invert_sliding_window
+from invert import invert_ebb, invert_msp, invert_sliding_window, coregister
 from util import matlab_context
 
 
-def model_comparison(nas, lpa, rpa, mri_fname, mesh_fnames, data_fname, patch_size=5, n_temp_modes=4, foi=[0, 256],
-                     woi=[-np.inf, np.inf], method='EBB', priors=[], n_folds=1, ideal_pc_test=0, mat_eng=None):
+def model_comparison(nas, lpa, rpa, mri_fname, mesh_fnames, data_fname, patch_size=5, n_temp_modes=4, foi=None,
+                     woi=None, method='EBB', priors=None, n_folds=1, ideal_pc_test=0,
+                     gain_mat_fnames=None, mat_eng=None):
     """
     Compare model fits using different meshes by computing the free energy.
 
@@ -27,6 +28,8 @@ def model_comparison(nas, lpa, rpa, mri_fname, mesh_fnames, data_fname, patch_si
     priors (list, optional): Indices of vertices to be used as priors (only for MSP). Default is an empty list.
     n_folds (int): Number of cross validation folds. Must be >1 for cross validation error
     ideal_pc_test (float): Percentage of channels to leave out (ideal because need an integer number of channels)
+    gain_mat_fnames (list, optional): List of filenames of the precomputed gain matrices, one for each mesh in
+                                      mesh_fnames. If None, they will be computed. Default is None
     mat_eng (matlab.engine.MatlabEngine, optional): Instance of MATLAB engine. Default is None.
 
     Returns:
@@ -39,25 +42,42 @@ def model_comparison(nas, lpa, rpa, mri_fname, mesh_fnames, data_fname, patch_si
     - If `mat_eng` is not provided, the function will start a new MATLAB engine instance.
     - The function will automatically close the MATLAB engine if it was started within the function.
     """
+    if priors is None:
+        priors = []
+    if woi is None:
+        woi = [-np.inf, np.inf]
+    if foi is None:
+        foi = [0, 256]
+    if gain_mat_fnames is None:
+        gain_mat_fnames=[None for _ in mesh_fnames]
+
     f_vals = []
     cv_errs = []
     with matlab_context(mat_eng) as eng:
-        for mesh_fname in mesh_fnames:
+        for l_idx, mesh_fname in enumerate(mesh_fnames):
+            coregister(nas, lpa, rpa, mri_fname, mesh_fname, data_fname)
             if method == 'EBB':
-                [f_val, cv_err] = invert_ebb(nas, lpa, rpa, mri_fname, mesh_fname, data_fname, 1,
-                                             patch_size=patch_size, n_temp_modes=n_temp_modes, foi=foi, woi=woi,
-                                             n_folds=n_folds, ideal_pc_test=ideal_pc_test, mat_eng=eng)
+                [f_val, cv_err] = invert_ebb(mesh_fname, data_fname, 1, patch_size=patch_size,
+                                             n_temp_modes=n_temp_modes, foi=foi, woi=woi, n_folds=n_folds,
+                                             ideal_pc_test=ideal_pc_test, gain_mat_fname=gain_mat_fnames[l_idx],
+                                             mat_eng=eng)
             elif method == 'MSP':
-                [f_val, cv_err] = invert_msp(nas, lpa, rpa, mri_fname, mesh_fname, data_fname, 1,
-                                             priors=priors, patch_size=patch_size, n_temp_modes=n_temp_modes, foi=foi,
-                                             woi=woi, n_folds=n_folds, ideal_pc_test=ideal_pc_test, mat_eng=eng)
+                [f_val, cv_err] = invert_msp(mesh_fname, data_fname, 1, priors=priors, patch_size=patch_size,
+                                             n_temp_modes=n_temp_modes, foi=foi, woi=woi, n_folds=n_folds,
+                                             ideal_pc_test=ideal_pc_test, gain_mat_fname=gain_mat_fnames[l_idx],
+                                             mat_eng=eng)
             f_vals.append(f_val)
             cv_errs.append(cv_err)
+
+    f_vals = np.array(f_vals)
+    cv_errs = np.array(cv_errs)
+
     return f_vals, cv_errs
 
 
 def sliding_window_model_comparison(prior, nas, lpa, rpa, mri_fname, mesh_fnames, data_fname, patch_size=5,
-                                    n_temp_modes=1, win_size=10, win_overlap=True, mat_eng=None):
+                                    n_temp_modes=1, win_size=10, win_overlap=True, hann=True, gain_mat_fnames=None,
+                                    mat_eng=None):
     """
     Compare model fits across different meshes using a sliding window approach.
 
@@ -77,6 +97,9 @@ def sliding_window_model_comparison(prior, nas, lpa, rpa, mri_fname, mesh_fnames
     win_size (int, optional): Size of the sliding window in samples. Default is 10. If you increase win_size, you may
                               have to increase n_temp_modes.
     win_overlap (bool, optional): Whether the windows should overlap. Default is True.
+    hann (bool, optional): Whether or not to use Hann windowing. Default is True
+    gain_mat_fnames (list, optional): List of filenames of the precomputed gain matrices, one for each mesh in
+                                      mesh_fnames. If None, they will be computed. Default is None
     mat_eng (matlab.engine.MatlabEngine, optional): Instance of MATLAB engine. Default is None.
 
     Returns:
@@ -89,12 +112,21 @@ def sliding_window_model_comparison(prior, nas, lpa, rpa, mri_fname, mesh_fnames
     - The function will automatically close the MATLAB engine if it was started within the function.
     - The prior index is adjusted by adding 1 to align with MATLAB's 1-based indexing.
     """
+    if gain_mat_fnames is None:
+        gain_mat_fnames=[None for _ in mesh_fnames]
+
     f_vals = []
     wois = []
     with matlab_context(mat_eng) as eng:
-        for mesh_fname in mesh_fnames:
-            [mesh_fvals, wois] = invert_sliding_window(prior, nas, lpa, rpa, mri_fname, mesh_fname, data_fname,
-                                                       1, patch_size=patch_size, n_temp_modes=n_temp_modes,
-                                                       win_size=win_size, win_overlap=win_overlap, mat_eng=eng)
+        for l_idx, mesh_fname in enumerate(mesh_fnames):
+            coregister(nas, lpa, rpa, mri_fname, mesh_fname, data_fname)
+
+            [mesh_fvals, wois] = invert_sliding_window(prior, mesh_fname, data_fname, 1, patch_size=patch_size,
+                                                       n_temp_modes=n_temp_modes, win_size=win_size,
+                                                       win_overlap=win_overlap, hann=hann,
+                                                       gain_mat_fname=gain_mat_fnames[l_idx], mat_eng=eng)
             f_vals.append(mesh_fvals)
+
+    f_vals=np.array(f_vals)
+
     return f_vals, wois
--- matlab/simulate.m
+++ matlab/simulate.m
@@ -1,5 +1,6 @@
 function sim_file=simulate(data_file, prefix, sim_vertices, sim_woi,...
-    sim_signals, dipole_orientations, dipole_moments, sim_patch_sizes, SNR, spm_path)
+    sim_signals, dipole_orientations, dipole_moments, sim_patch_sizes, SNR,...
+    average, spm_path)
 
 addpath(spm_path);
 
@@ -15,3 +16,16 @@ Dmesh=spm_eeg_load(data_file);
 
 [a1, ~, ~]=fileparts(data_file);
 sim_file=fullfile(a1, Dnew.fname);
+
+if average
+    clear jobs
+    matlabbatch={};
+    batch_idx=1;
+
+    matlabbatch{batch_idx}.spm.meeg.averaging.average.D = {sim_file};
+    matlabbatch{batch_idx}.spm.meeg.averaging.average.userobust.standard = false;
+    matlabbatch{batch_idx}.spm.meeg.averaging.average.plv = false;
+    matlabbatch{batch_idx}.spm.meeg.averaging.average.prefix = 'm';
+    spm_jobman('run', matlabbatch);
+    sim_file=fullfile(a1, sprintf('m%s',Dnew.fname));
+end
