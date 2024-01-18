f531af03fb554b3727496531d192dfa713cf710b
jbonaiuto <jbonaiuto@gmail.com>
2024-01-18 08:15:45 +0100

    added required libraries to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from setuptools import setup
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
+
+# Define your extension module
+extensions = [
+    Extension(
+        name="laMEG.csurf",
+        sources=["csurf.pyx"],
+        # add any necessary compile-time flags here
+    )
+]
 
 setup(
     name='laMEG',
@@ -11,8 +20,12 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/danclab/laMEG',
     install_requires=open('requirements.txt').read().splitlines(),
-    ext_modules=cythonize("csurf.pyx"),
-    package_data={'laMEG': ['matlab/*.m']},
+    packages=find_packages(),
+    ext_modules=cythonize(extensions),
+    package_data={
+        # Include any package data files here
+        'laMEG': ['*.so', 'matlab/*.m'],
+    },
     include_package_data=True,
     zip_safe=False,
 )
