bbef5fe26e99051718db4e7612d6b68999ad63ee
jbonaiuto <jbonaiuto@gmail.com>
2024-01-22 14:01:59 +0100

    return channel labels with data
    Former-commit-id: 841afc4c19f7b2912ee7eb914913a616302f4af5

--- lameg/invert.py
+++ lameg/invert.py
@@ -365,7 +365,7 @@ def load_source_time_series(data_fname, mu_matrix=None, inv_fname=None, vertices
             )
             source_ts = np.array(source_ts)
     else:
-        sensor_data = load_meg_sensor_data(data_fname, mat_eng=mat_eng)
+        sensor_data, _ = load_meg_sensor_data(data_fname, mat_eng=mat_eng)
         v_idx = np.arange(mu_matrix.shape[0])
         if len(vertices):
             v_idx = np.array(vertices)
