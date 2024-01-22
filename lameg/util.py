83e94cf4848fa68996989fde5ae45f65c70f3288
jbonaiuto <jbonaiuto@gmail.com>
2024-01-22 08:00:54 +0100

    added get_layer_names function

--- lameg/util.py
+++ lameg/util.py
@@ -81,3 +81,36 @@ def load_meg_sensor_data(data_fname, mat_eng=None):
         )
 
     return np.array(sensor_data)
+
+
+def get_surface_names(n_layers, surf_path, orientation_method):
+    """
+    Generates a list of filenames for each mesh layer in a multi-layer mesh setup.
+
+    Parameters:
+    - n_layers (int): The number of layers in the mesh.
+    - surf_path (str): The file path where the mesh files are located.
+    - orientation_method (str): The method used for orientation in the naming of mesh files.
+
+    Returns:
+    - list: A list of strings, where each string is the full file path to a mesh layer file. The list
+            order corresponds to the layers' order, starting from the outermost layer (pial surface)
+            to the innermost layer (white matter surface).
+
+    This function assumes a specific naming convention for the mesh files. The outermost layer is
+    named as 'pial', the innermost as 'white', and the intermediate layers are named based on their
+    relative position between 1 (pial) and 0 (white), with the position formatted to three decimal places.
+    Each filename also includes an orientation method specifier.
+    """
+    # Get name of each mesh that makes up the layers of the multilayer mesh
+    layers = np.linspace(1, 0, n_layers)
+    layer_fnames = []
+    for l, layer in enumerate(layers):
+        if layer == 1:
+            layer_fnames.append(os.path.join(surf_path, f'pial.ds.{orientation_method}.gii'))
+        elif layer > 0 and layer < 1:
+            layer_name = '{:.3f}'.format(layer)
+            layer_fnames.append(os.path.join(surf_path, f'{layer_name}.ds.{orientation_method}.gii'))
+        elif layer == 0:
+            layer_fnames.append(os.path.join(surf_path, f'white.ds.{orientation_method}.gii'))
+    return layer_fnames
