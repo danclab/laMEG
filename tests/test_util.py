f1fdd10be0207f46cd9a6c50b13cfe1d241f288c
jbonaiuto <jbonaiuto@gmail.com>
2024-07-09 20:24:25 +0200

    added test_get_fiducial_coords
    Former-commit-id: 3637edd34bababaed5c9d0e0bbcb8aa3f116bd24

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,9 +1,11 @@
 """
 This module contains the unit tests for the `utils` module from the `lameg` package.
 """
+from unittest.mock import mock_open, patch
+
 import numpy as np
 
-from lameg.util import check_many, spm_context, big_brain_proportional_layer_boundaries
+from lameg.util import check_many, spm_context, big_brain_proportional_layer_boundaries, get_fiducial_coords
 from spm import spm_standalone
 
 
@@ -163,3 +165,44 @@ def test_big_brain_proportional_layer_boundaries():
     assert bb_data['rh'].shape[0] == 6 and bb_data['rh'].shape[1] == 163842
     expected = np.array([0.07103447, 0.15451714, 0.46817848, 0.53011256, 0.7344828, 1.])
     assert np.max(np.abs(bb_data['rh'][:, 0] - expected)) < 1e-6
+
+
+def test_get_fiducial_coords():
+    """
+    Tests the `get_fiducial_coords` function to ensure it correctly fetches and parses fiducial
+    coordinates from a TSV file.
+
+    The function should correctly parse the NAS, LPA, and RPA coordinates as lists of floats for a
+    given subject ID.
+    """
+    # Sample data to mimic what might be found in the TSV file
+    tsv_data = "subj_id\tnas\tlpa\trpa\n" \
+               "subj1\t1.0,2.0,3.0\t4.0,5.0,6.0\t7.0,8.0,9.0\n" \
+               "subj2\t10.0,11.0,12.0\t13.0,14.0,15.0\t16.0,17.0,18.0\n"
+
+    # Use mock_open to simulate file reading operations
+    m = mock_open(read_data=tsv_data)
+
+    with patch('builtins.open', m):
+        nas, lpa, rpa = get_fiducial_coords('subj1', 'dummy_filename.tsv')
+
+    # Assert the expected outputs
+    assert nas == [1.0, 2.0, 3.0], "NAS coordinates do not match expected values"
+    assert lpa == [4.0, 5.0, 6.0], "LPA coordinates do not match expected values"
+    assert rpa == [7.0, 8.0, 9.0], "RPA coordinates do not match expected values"
+
+    # Also, you might want to test the case where the subject ID is not found
+    with patch('builtins.open', m):
+        nas, lpa, rpa = get_fiducial_coords('subj3', 'dummy_filename.tsv')
+
+    assert nas is None and lpa is None and rpa is None, ("Should return None for all coordinates if"
+                                                         " the subject ID is not found")
+
+    nas, lpa, rpa = get_fiducial_coords('sub-104', './test_data/participants.tsv')
+    nas_target = np.array([0.9662503311032098, 108.83514306876269, 1.6712361927090313])
+    lpa_target = np.array([-74.28671169006893, 20.15061014698176, -29.849056272705948])
+    rpa_target = np.array([76.02110531729883, 18.9467849625573, -25.779407159603114])
+
+    assert np.sum(np.abs(nas - nas_target)) < 1e-6
+    assert np.sum(np.abs(lpa - rpa_target)) < 1e-6
+    assert np.sum(np.abs(lpa - rpa_target)) < 1e-6
