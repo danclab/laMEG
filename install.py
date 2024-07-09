6c2f976d431242c69a742c8aad6aaf9ec2437a84
jbonaiuto <jbonaiuto@gmail.com>
2024-07-09 18:53:40 +0200

    download and extract test data, cache test data and matlab runtime
    Former-commit-id: 94e37a60a502b8e0ae549cd4cb07cd4ce914cf30

--- install.py
+++ install.py
@@ -23,12 +23,12 @@ def install_package():
     subprocess.check_call([sys.executable, "-m", "pip", "install", "."])
 
 
-def install_spm():
+def install_spm(base_dir):
     """
     Installs the SPM standalone package from a combined CTF file,
     assumes that the SPM standalone package is present in a subdirectory relative to this script.
     """
-    spm_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'spm')
+    spm_dir = os.path.join(base_dir, 'spm')
 
     # Change to the directory containing the spm package standalone
     if not os.path.exists(os.path.join(spm_dir, 'spm_standalone', 'spm_standalone.ctf')):
@@ -44,20 +44,25 @@ def install_spm():
     # Install the spm package
     subprocess.check_call([sys.executable, "setup.py", "install"])
     # Change back to the original directory
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(base_dir)
 
 
-def download_matlab_runtime(url, save_path):
+def download_file(url, save_path):
     """
-    Downloads the MATLAB runtime from a specified URL to a specified path,
-    ensuring the directory structure is created if it does not exist.
+    Downloads a file a specified URL to a specified path
     """
 
-    # Create the directory if it doesn't exist
-    os.makedirs(os.path.dirname(save_path), exist_ok=True)
     if not os.path.exists(save_path):
         # Download the file using wget
         subprocess.check_call(['wget', '-c', url, '-O', save_path])
+
+
+def extract_test_data(save_path, base_dir):
+    """
+    Extracts the test data from a .tar.gz file.
+    """
+    if os.path.exists(save_path) and not os.path.exists(os.path.join(base_dir, '/test_data')):
+        subprocess.check_call(['tar', '-xzvf', save_path])
 
 
 def extract_matlab_runtime(zip_path, extract_to):
@@ -175,10 +180,21 @@ def main():
     """
     parser = argparse.ArgumentParser(description=('Set up the required environment and '
                                                  'dependencies.'))
-    parser.add_argument('--no_matlab_runtime', action='store_true',
-                        help='Skip downloading and installing the MATLAB Runtime.')
+    parser.add_argument(
+        '--no_matlab_runtime',
+        action='store_true',
+        help='Skip downloading and installing the MATLAB Runtime.'
+    )
+
+    parser.add_argument(
+        '--no_test_data',
+        action='store_true',
+        help='Skip downloading test data.'
+    )
 
     args = parser.parse_args()
+
+    base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
     # Install the package first
     install_package()
@@ -187,14 +203,13 @@ def main():
     importlib.invalidate_caches()
 
     # Install standalone SPM
-    install_spm()
+    install_spm(base_dir)
 
     if not args.no_matlab_runtime:
         # Attempt to locate the installed package directory
-        package_dir = get_installed_package_dir('lameg')
+        package_dir = get_installed_package_dir('spm_standalone')
 
-        matlab_runtime_zip = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                          'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip')
+        matlab_runtime_zip = os.path.join(base_dir, 'MATLAB_Runtime_R2019a_Update_9_glnxa64.zip')
         matlab_download_url = (
             'https://ssd.mathworks.com/supportfiles/downloads/R2019a/Release/9/'
             'deployment_files/installer/complete/glnxa64/'
@@ -202,7 +217,7 @@ def main():
         )
 
         # Download MATLAB Runtime
-        download_matlab_runtime(matlab_download_url, matlab_runtime_zip)
+        download_file(matlab_download_url, matlab_runtime_zip)
         matlab_runtime_extract_dir = os.path.join(package_dir, 'matlab_runtime')
 
         conda_env_path = os.path.dirname(os.path.dirname(sys.executable))
@@ -218,6 +233,18 @@ def main():
         create_deactivate_script(conda_env_path)
 
         os.remove(matlab_runtime_zip)
+
+    if not args.no_test_data:
+        test_data_zip = os.path.join(base_dir, 'test_data.tar.gz')
+        test_data_download_url = 'https://osf.io/fh9us/download'
+
+        # Download test data
+        download_file(test_data_download_url, test_data_zip)
+
+        # Extract test data
+        extract_test_data(test_data_zip)
+
+
 
 
 if __name__ == "__main__":
