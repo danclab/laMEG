5809e373d47b5e1e9aca8a59fad8262869ac4ebc
jbonaiuto <jbonaiuto@gmail.com>
2024-07-15 13:11:36 +0200

    updated test_data to include FWHM file for multilayer.2 and removed test of smooth_multilayer_mesh_mm
    Former-commit-id: 613a78d56be1760bf5e300a9ca835b4f7bd551bc

--- tests/test_01_surf.py
+++ tests/test_01_surf.py
@@ -1,13 +1,9 @@
 """
 This module contains the unit tests for the `surf` module from the `lameg` package.
 """
-import os
-
 import numpy as np
-import pytest
-from scipy.io import loadmat
 
-from lameg.surf import split_fv, smoothmesh_multilayer_mm  # pylint: disable=no-name-in-module
+from lameg.surf import split_fv  # pylint: disable=no-name-in-module
 
 
 def test_split_fv():
@@ -113,63 +109,3 @@ def test_split_fv():
                        [5, 2],
                        [5, 0]])
     assert np.allclose(patch_vertices, target)
-
-
-@pytest.mark.dependency()
-def test_smoothmesh_multilayer_mm():
-    """
-    Tests the `smoothmesh_multilayer_mm` function to ensure it correctly smooths the surface mesh
-    data and saves the output as expected. The test verifies the function's ability to handle file
-    operations correctly and produce a matrix of smoothed values that match a predefined target.
-
-    This test primarily checks:
-    1. The function's capability to execute smoothing operations on multilayer mesh data.
-    2. The creation of an output file following the smoothing process.
-    3. The correctness of the output data by comparing the contents of the newly created file
-       against expected values.
-
-    Steps performed:
-    - Run the `smoothmesh_multilayer_mm` function with specified parameters to perform the
-      smoothing.
-    - Check for the existence of the output file post-function execution to ensure file operations
-      are handled correctly.
-    - Load the output file and validate the content against a target matrix to ensure the smoothing
-      process is accurate.
-
-    Assertions:
-    - Assert that the output file exists after function execution.
-    - Assert that the numerical content of the output file closely matches the predefined target
-      values, confirming both the smoothing accuracy and the correctness of file writing
-      operations.
-    """
-
-    test_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../test_data')
-    subj_id = 'sub-104'
-    # pylint: disable=duplicate-code
-    mesh_fname = os.path.join(
-        test_data_path,
-        subj_id,
-        'surf',
-        'multilayer.2.ds.link_vector.fixed.gii'
-    )
-
-    out_fname = os.path.join(
-        test_data_path,
-        subj_id,
-        'surf',
-        'FWHM5.00_multilayer.2.ds.link_vector.fixed.mat'
-    )
-
-    smoothmesh_multilayer_mm(mesh_fname, 5, 2)
-
-    assert os.path.exists(out_fname)
-
-    results = loadmat(out_fname)
-    target = np.array(
-        [[0.17707503, 0.12816778, 0.04624505, 0.01718994, 0., 0., 0., 0., 0.13324403 ,0.],
-         [0.16848713, 0.23277974, 0.1643145,  0.09286189, 0., 0., 0., 0., 0.06913673, 0.],
-         [0.05768403, 0.15591159, 0.22087557, 0.19283595, 0., 0., 0., 0., 0., 0.],
-         [0.01843762, 0.07576701, 0.16581663, 0.18992747, 0., 0., 0.,0., 0., 0.],
-         [0., 0., 0., 0., 0.24208555, 0.20217255, 0.12082949, 0., 0., 0.]]
-    )
-    assert np.allclose(results['QG'][:5,:10].todense(), target)
