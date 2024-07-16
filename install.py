b85e31e49144cb5bf3ec5956fee8ace02843da52
jbonaiuto <jbonaiuto@gmail.com>
2024-07-16 15:16:50 +0200

    fixed line length issue
    Former-commit-id: 0a4cd02a29ba3dfda97ec9bdc7bd8e2a368097ac

--- install.py
+++ install.py
@@ -20,7 +20,14 @@ def install_package():
     Installs the current directory as a pip package.
     This function assumes the directory contains a setup.py file.
     """
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy==1.19.5', 'Cython==3.0.10'])
+    subprocess.check_call([
+        sys.executable,
+        '-m',
+        'pip',
+        'install',
+        'numpy==1.19.5',
+        'Cython==3.0.10'
+    ])
     subprocess.check_call([sys.executable, 'setup.py', 'build_ext', '--inplace', '-v'])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "."])
 
