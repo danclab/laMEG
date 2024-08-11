45c886f3e4393e0aec90fb605ee26ce40119d458
jbonaiuto <jbonaiuto@gmail.com>
2024-08-11 12:30:43 +0200

    incremented version number

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ with open('README.md', 'r', encoding="utf-8") as f:
 
 setup(
     name='lameg',
-    version='0.0.1',
+    version='0.0.2',
     description='A toolbox for laminar inference with MEG',
     long_description=long_description,
     long_description_content_type='text/markdown',
