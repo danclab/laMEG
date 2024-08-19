87fb44006e3e8a510bd54d521651cea8ad49cf69
jbonaiuto <jbonaiuto@gmail.com>
2024-08-19 17:30:22 +1000

    incremented version

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ with open('README.rst', 'r', encoding="utf-8") as f:
 
 setup(
     name='lameg',
-    version='0.0.2',
+    version='0.0.3',
     description='A toolbox for laminar inference with MEG',
     long_description=long_description,
     long_description_content_type='text/x-rst',
