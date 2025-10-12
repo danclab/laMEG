6f580fef64d73cb315ee8dbeb46cbbfb45978f3a
jbonaiuto <jbonaiuto@gmail.com>
2025-10-12 10:27:29 +0200

    Dont plot layer boundaries if None

--- lameg/viz.py
+++ lameg/viz.py
@@ -334,8 +334,9 @@ def plot_csd(csd, times, axis, colorbar=True, cmap="RdBu_r", vmin_vmax=None, n_l
         aspect="auto", extent=extent,
         cmap=cmap, interpolation="none"
     )
-    for boundary in layer_boundaries:
-        axis.axhline(y=boundary, color='k', linestyle='--')
+    if layer_boundaries is not None:
+        for boundary in layer_boundaries:
+            axis.axhline(y=boundary, color='k', linestyle='--')
 
     axis.set_ylim(1, 0)
     axis.set_yticks(np.linspace(0, 1, n_layers))
